dc681f1d8d2fffb76eefd5c5243f05ed486b608f
silenceawp <46513756+silenceawp@users.noreply.github.com>
2020-05-07 15:43:36 +0800

    bullet update

--- data.py
+++ data.py
@@ -1,3 +1,11 @@
 # Do data processing in this file
-
+import cv2
 import option
+def max_huidu(input_path):
+    return 0
+def max_size(input_path):
+    return 0
+if __name__ == '__main__':
+    img=cv2.imread("D:\\Second semester of sophomore year\\ML\\term_project\\data\\train\\0.png")
+    cv2.imshow("0",img)
+    cv2.waitKey()
