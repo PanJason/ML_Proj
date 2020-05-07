6d7e405fedc52bbe34314d76a1c46860caa889e9
silenceawp <46513756+silenceawp@users.noreply.github.com>
2020-05-07 20:05:37 +0800

    data_process

--- data.py
+++ data.py
@@ -1,11 +1,16 @@
 # Do data processing in this file
 import cv2
 import option
-def max_huidu(input_path):
-    return 0
-def max_size(input_path):
-    return 0
-if __name__ == '__main__':
-    img=cv2.imread("D:\\Second semester of sophomore year\\ML\\term_project\\data\\train\\0.png")
-    cv2.imshow("0",img)
-    cv2.waitKey()
+import numpy as np
+
+def dataprocess(input_img):
+    padding_size=(3056,3056,3)
+    output_img=np.zeros(padding_size)
+    w=min(padding_size[0],input_img.shape[0])
+    h=min(padding_size[1],input_img.shape[1])
+    output_img[0:w,0:h]=input_img[0:w,0:h]
+
+    huidu=160
+    output_img=output_img*160/np.mean(input_img)
+    return output_img
+
